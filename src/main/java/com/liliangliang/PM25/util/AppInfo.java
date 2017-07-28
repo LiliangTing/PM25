2c3b9f70bc661881a36fe759bdb6a66b28a2c015
zk123 <zkdcloud_cn@163.com>
2017-07-28 10:55:33 +0800

    add logback

--- src/main/java/com/liliangliang/PM25/util/AppInfo.java
+++ src/main/java/com/liliangliang/PM25/util/AppInfo.java
@@ -2,7 +2,7 @@ package com.liliangliang.PM25.util;
 
 public class AppInfo {
 	
-	public static final String url="	http://www.pm25.in/api/querys/aqis_by_station.json?tocken=";
+	public static final String url="http://www.pm25.in/api/querys/aqis_by_station.json?tocken=";
 	public static final String tocken="5j1znBVAsnSf5xQyNQyq";
 
 }
