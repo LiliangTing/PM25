394324fb181120a586c8576ce3bc8e0fd299cd12
LiliangTing <liliang941107@sina.com>
2017-07-28 09:09:26 +0800

    [数据收集完成]

--- src/main/java/com/liliangliang/PM25/util/AppInfo.java
+++
@@ -1,8 +0,0 @@
-package com.liliangliang.PM25.util;
-
-public class AppInfo {
-	
-	public static final String url="	http://www.pm25.in/api/querys/aqis_by_station.json?tocken=";
-	public static final String tocken="5j1znBVAsnSf5xQyNQyq";
-
-}
