f8bd0ddc29dc1724d82b5870ddf22734ac41f26f
lianglianglee <liangliang_li@fengzhiding.com>
2018-10-24 17:05:02 +0800

    开始做历史数据查询

--- src/main/java/com/liliangliang/core/dao/CityAirDAO.java
+++ src/main/java/com/liliangliang/core/dao/CityAirDAO.java
@@ -1,5 +1,6 @@
 package com.liliangliang.core.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import com.liliangliang.core.entry.CityAir;
@@ -11,5 +12,8 @@ public interface CityAirDAO {
   List<CityAir> getNew();
 
   List<MapData> getNewMapData();
+
+
+  List<CityAir> getByDate(Date date);
 
 }
--- src/main/java/com/liliangliang/core/service/CityAirService.java
+++ src/main/java/com/liliangliang/core/service/CityAirService.java
@@ -1,13 +1,42 @@
 package com.liliangliang.core.service;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.SimpleFormatter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.liliangliang.core.dto.MapData;
 
+import com.liliangliang.core.dao.CityAirDAO;
 import com.liliangliang.core.entry.CityAir;
 
-public interface CityAirService {
+@Service("cityAirService")
+public class CityAirService {
+  @Autowired
+  private CityAirDAO cityAirDAO;
 
-	List<CityAir> getByNew();
-	
-	List<MapData> getMapByNew();
+  public List<CityAir> getByNew() {
+    return this.cityAirDAO.getNew();
+  }
+
+  public List<MapData> getMapByNew() {
+    return this.cityAirDAO.getNewMapData();
+  }
+
+  public  List<CityAir> getByDate(String date){
+    DateFormat formatter=new SimpleDateFormat("yyyy-MM-dd HH");
+    try {
+      Date date1 = formatter.parse(date);
+      return cityAirDAO.getByDate(date1);
+    }catch (ParseException e){
+      e.printStackTrace();
+      return new ArrayList<>();
+    }
+
+  }
 }
